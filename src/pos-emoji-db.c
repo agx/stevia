ed312bbff8228ef92ed462325068eec808d9478d
Guido Günther <agx@sigxcpu.org>
2025-05-07 23:05:35 +0200

    emoji-db: Drop unused property
    Fixes 39d130241 ("emoji-db: New class to hold emojis")
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>

--- src/pos-emoji-db.c
+++ src/pos-emoji-db.c
@@ -20,58 +20,12 @@
  * The emoji database
  */
 
-enum {
-  PROP_0,
-  PROP_DUMMY,
-  PROP_LAST_PROP
-};
-static GParamSpec *props[PROP_LAST_PROP];
-
 struct _PosEmojiDb {
   GObject   parent;
 
-  gboolean  dummy;
   GVariant *emoji_data;
 };
 G_DEFINE_TYPE (PosEmojiDb, pos_emoji_db, G_TYPE_OBJECT)
-
-
-static void
-pos_emoji_db_set_property (GObject      *object,
-                           guint         property_id,
-                           const GValue *value,
-                           GParamSpec   *pspec)
-{
-  PosEmojiDb *self = POS_EMOJI_DB (object);
-
-  switch (property_id) {
-  case PROP_DUMMY:
-    self->dummy = g_value_get_boolean (value);
-    break;
-  default:
-    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
-    break;
-  }
-}
-
-
-static void
-pos_emoji_db_get_property (GObject    *object,
-                           guint       property_id,
-                           GValue     *value,
-                           GParamSpec *pspec)
-{
-  PosEmojiDb *self = POS_EMOJI_DB (object);
-
-  switch (property_id) {
-  case PROP_DUMMY:
-    g_value_set_boolean (value, self->dummy);
-    break;
-  default:
-    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
-    break;
-  }
-}
 
 
 static void
@@ -90,16 +44,7 @@ pos_emoji_db_class_init (PosEmojiDbClass *klass)
 {
   GObjectClass *object_class = G_OBJECT_CLASS (klass);
 
-  object_class->get_property = pos_emoji_db_get_property;
-  object_class->set_property = pos_emoji_db_set_property;
   object_class->finalize = pos_emoji_db_finalize;
-
-  props[PROP_DUMMY] =
-    g_param_spec_boolean ("dummy", "", "",
-                          FALSE,
-                          G_PARAM_READWRITE | G_PARAM_EXPLICIT_NOTIFY);
-
-  g_object_class_install_properties (object_class, PROP_LAST_PROP, props);
 }
 
 
