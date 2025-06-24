83f6a9d113c5b84fdbabf7737d17c6852abe1da3
Guido Günther <agx@sigxcpu.org>
2025-06-24 17:11:35 +0200

    tests: Don't leak display name
    Signed-off-by: Guido Günther <agx@sigxcpu.org>

--- tests/pos-test-completer.h
+++ tests/pos-test-completer.h
@@ -9,8 +9,10 @@
 G_BEGIN_DECLS
 
 #define pos_completer_assert_initial_state(c, n, d) G_STMT_START { \
+  g_autofree char *display_name = NULL; \
   g_assert_cmpstr (pos_completer_get_name (c), ==, n); \
-  g_assert_cmpstr (pos_completer_get_display_name (c), ==, d); \
+  display_name = pos_completer_get_display_name (c); \
+  g_assert_cmpstr (display_name, ==, d); \
   g_assert_cmpstrv (pos_completer_get_completions (c), NULL); \
   g_assert_cmpstr (pos_completer_get_preedit (c), ==, ""); \
   g_assert_cmpstr (pos_completer_get_before_text (c), ==, ""); \
