agx/stevia

commit a849b11ded1ce676e72dfa37dea3f665567b48b3

hunspell: Check for emojis too by using PosCompleterBase
Signed-off-by: Guido Günther <agx@sigxcpu.org>

src/completers/pos-completer-hunspell.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2022 Purism SPC
+ *               2025 The Phosh Developers
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *
@@ -10,6 +11,7 @@
 
 #include "pos-config.h"
 
+#include "pos-completer-base.h"
 #include "pos-completer-priv.h"
 #include "pos-completer-hunspell.h"
 
@@ -18,6 +20,7 @@
 #include <hunspell.h>
 
 #define MAX_COMPLETIONS 3
+#define MAX_ADDITIONAL_RESULTS 3
 
 enum {
   PROP_0,
@@ -39,7 +42,7 @@ static GParamSpec *props[PROP_LAST_PROP];
  * based on typo corrections.
  */
 struct _PosCompleterHunspell {
-  GObject               parent;
+  PosCompleterBase      parent;
 
   char                 *name;
   GString              *preedit;
@@ -53,7 +56,7 @@ struct _PosCompleterHunspell {
 static void pos_completer_hunspell_interface_init (PosCompleterInterface *iface);
 static void pos_completer_hunspell_initable_interface_init (GInitableIface *iface);
 
-G_DEFINE_TYPE_WITH_CODE (PosCompleterHunspell, pos_completer_hunspell, G_TYPE_OBJECT,
+G_DEFINE_TYPE_WITH_CODE (PosCompleterHunspell, pos_completer_hunspell, POS_TYPE_COMPLETER_BASE,
                          G_IMPLEMENT_INTERFACE (POS_TYPE_COMPLETER,
                                                 pos_completer_hunspell_interface_init)
                          G_IMPLEMENT_INTERFACE (G_TYPE_INITABLE,
@@ -63,7 +66,17 @@ static void
 pos_completer_hunspell_take_completions (PosCompleter *iface, GStrv completions)
 {
   PosCompleterHunspell *self = POS_COMPLETER_HUNSPELL (iface);
+  g_auto (GStrv) additional_results = NULL;
+  g_autoptr (GStrvBuilder) builder = g_strv_builder_new ();
 
+  additional_results = pos_completer_base_get_additional_results (POS_COMPLETER_BASE (self),
+                                                                  self->preedit->str,
+                                                                  MAX_ADDITIONAL_RESULTS);
+
+  if (completions)
+    g_strv_builder_addv (builder, (const char **)completions);
+  if (additional_results)
+    g_strv_builder_addv (builder, (const char **)additional_results);
   g_strfreev (self->completions);
   self->completions = completions;
 

src/completers/pos-completer-hunspell.h
@@ -1,11 +1,12 @@
 /*
- * Copyright (C) 2023 The Phosh Developers
+ * Copyright (C) 2023-2025 The Phosh Developers
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 #pragma once
 
+#include "pos-completer-base.h"
 #include "pos-completer.h"
 
 #include <glib-object.h>
@@ -14,7 +15,8 @@ G_BEGIN_DECLS
 
 #define POS_TYPE_COMPLETER_HUNSPELL (pos_completer_hunspell_get_type ())
 
-G_DECLARE_FINAL_TYPE (PosCompleterHunspell, pos_completer_hunspell, POS, COMPLETER_HUNSPELL, GObject)
+G_DECLARE_FINAL_TYPE (PosCompleterHunspell, pos_completer_hunspell, POS, COMPLETER_HUNSPELL,
+                      PosCompleterBase)
 
 PosCompleter *pos_completer_hunspell_new (GError **error);
 gboolean      pos_completer_hunspell_find_dict (const char  *lang,

commit 340e832b76963be871baeb017a0742f988a0b1a8

hunspell-completer: Fix additional completions
We used the old completer rather than the new one from the builder.
While at that make the presage and hunspell completers more look
alike by using `GStrvBuilder` everywhere.

Fixes: a849b11de ("hunspell: Check for emojis too by using PosCompleterBase")
Signed-off-by: Guido Günther <agx@sigxcpu.org>

src/completers/pos-completer-hunspell.c
@@ -65,22 +65,25 @@ G_DEFINE_TYPE_WITH_CODE (PosCompleterHunspell, pos_completer_hunspell, POS_TYPE_
                                                 pos_completer_hunspell_initable_interface_init))
 
 static void
-pos_completer_hunspell_take_completions (PosCompleter *iface, GStrv completions)
+pos_completer_hunspell_set_completions (PosCompleter *iface,
+                                        GStrv         completions,
+                                        gboolean      additional_sources)
 {
   PosCompleterHunspell *self = POS_COMPLETER_HUNSPELL (iface);
   g_auto (GStrv) additional_results = NULL;
   g_autoptr (GStrvBuilder) builder = g_strv_builder_new ();
 
-  additional_results = pos_completer_base_get_additional_results (POS_COMPLETER_BASE (self),
-                                                                  self->preedit->str,
-                                                                  MAX_ADDITIONAL_RESULTS);
+  if (additional_sources)
+    additional_results = pos_completer_base_get_additional_results (POS_COMPLETER_BASE (self),
+                                                                    self->preedit->str,
+                                                                    MAX_ADDITIONAL_RESULTS);
 
   if (completions)
     g_strv_builder_addv (builder, (const char **)completions);
   if (additional_results)
     g_strv_builder_addv (builder, (const char **)additional_results);
   g_strfreev (self->completions);
-  self->completions = completions;
+  self->completions = g_strv_builder_end (builder);
 
   g_object_notify_by_pspec (G_OBJECT (self), props[PROP_COMPLETIONS]);
 }
@@ -104,11 +107,10 @@ pos_completer_hunspell_set_preedit (PosCompleter *iface, const char *preedit)
     return;
 
   g_string_truncate (self->preedit, 0);
-  if (preedit) {
+  if (preedit)
     g_string_append (self->preedit, preedit);
-  } else {
-    pos_completer_hunspell_take_completions (POS_COMPLETER (self), NULL);
-  }
+  else
+    pos_completer_hunspell_set_completions (POS_COMPLETER (self), NULL, FALSE);
 
   g_object_notify_by_pspec (G_OBJECT (self), props[PROP_PREEDIT]);
 }
@@ -366,7 +368,8 @@ pos_completer_hunspell_feed_symbol (PosCompleter *iface, const char *symbol)
   PosCompleterHunspell *self = POS_COMPLETER_HUNSPELL (iface);
   g_autofree char *preedit = g_strdup (self->preedit->str);
   g_autofree char *input = NULL;
-  g_autoptr (GPtrArray) completions = g_ptr_array_new ();
+  g_autoptr (GStrvBuilder) builder = g_strv_builder_new ();
+  g_auto (GStrv) completions = NULL;
   char **suggestions;
   int ret;
 
@@ -391,7 +394,7 @@ pos_completer_hunspell_feed_symbol (PosCompleter *iface, const char *symbol)
 
   input = convert_from_utf8 (self, self->preedit->str);
   if (Hunspell_spell (self->handle, input))
-    g_ptr_array_add (completions, g_strdup (self->preedit->str));
+    g_strv_builder_add (builder, g_strdup (self->preedit->str));
 
   ret = Hunspell_suggest (self->handle, &suggestions, input);
   if (ret > 0) {
@@ -399,15 +402,14 @@ pos_completer_hunspell_feed_symbol (PosCompleter *iface, const char *symbol)
       char *utf8 = convert_to_utf8 (self, suggestions[i]);
 
       if (utf8)
-        g_ptr_array_add (completions, g_steal_pointer (&utf8));
+        g_strv_builder_take (builder, utf8);
     }
     Hunspell_free_list (self->handle, &suggestions, ret);
   }
 
-  g_ptr_array_add (completions, NULL);
+  completions = g_strv_builder_end (builder);
+  pos_completer_hunspell_set_completions (POS_COMPLETER (self), completions, TRUE);
 
-  pos_completer_hunspell_take_completions (POS_COMPLETER (self),
-                                           (char **)g_ptr_array_steal (completions, NULL));
   return TRUE;
 }
 